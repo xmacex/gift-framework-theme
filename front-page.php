f5742515787af0cdf57d3fbef254b9bbc7d53514
Mace Ojala <maco@itu.dk>
2018-11-07 18:12:17 +0100

    Added front page template

---
+++ front-page.php
@@ -0,0 +1,9 @@
+<?php get_header(); ?>
+
+<!-- Site Content Goes Here -->
+<?php while (have_posts()): the_post(); ?>
+    <section class="grid-page wrap-container">
+	<?php the_content(); ?>
+    </section>
+<?php endwhile; ?>
+<?php get_footer(); ?>
