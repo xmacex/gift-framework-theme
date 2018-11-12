a34ac96f06cff82da389889c91046ca55104c939
Tim Wray <timwray.mail@gmail.com>
2018-11-12 20:36:45 +0100

    Added styling to case studies template.

--- category.php
+++ category.php
@@ -2,9 +2,9 @@
 
 <section class="grid-page wrap-container">
     <div class="column-container grid-container">
-	<?php while (have_posts()): the_post(); ?>
+  <?php while (have_posts()): the_post(); ?>
 	    <div class="case-study grid-section">
-		<div class="faded image tile" style="background-image:url('<?php echo get_the_post_thumbnail_url(); ?>')"></div>
+		<div class="faded image tile stripes-slope-right" style="background-image:url('<?php echo get_the_post_thumbnail_url(); ?>')"></div>
 		<div class="tile">
 		    <h3><?php echo the_title(); ?></h3>
 		    <?php echo the_content('<p>Learn more&hellip;</p>'); ?>
