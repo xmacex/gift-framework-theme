32ce7e2f374d461b0f8ac259925239e5a7c90ed4
Tim Wray <timwray.mail@gmail.com>
2018-11-24 21:30:58 +0100

    Removed the 'We love open source' link.

--- footer.php
+++ footer.php
@@ -1,9 +1,6 @@
 <footer class="wrap-container">
     <div class="footer-container">
 	<div class="footer-container-inner">
-            <p>
-		We &hearts; open source. Learn more about how we used it in GIFT.
-            </p>
             <img class="eu-flag-footer" src="<?php echo get_stylesheet_directory_uri(); ?>/img/eu-flag-footer.png" alt="European Union Flag" />
             <p>
 		This project has received funding from the European Union’s Horizon 2020 research and innovation programme under grant agreement No 727040.
