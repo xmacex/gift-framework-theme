e1d00b5db4f646382bc902c10714fe86b371b363
Tim Wray <timwray.mail@gmail.com>
2019-02-27 20:16:44 +0100

    Slight formatting of page.php

--- page.php
+++ page.php
@@ -2,8 +2,8 @@
 
 <!-- Site Content Goes Here -->
 <?php while (have_posts()): the_post(); ?>
-    <article class="article-content wrap-container content-area">
-	<?php the_content(); ?>
-    </article>
+  <article class="article-content wrap-container content-area">
+    <?php the_content(); ?>
+  </article>
 <?php endwhile; ?>
 <?php get_footer(); ?>
