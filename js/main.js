b386eead85ac8acbd10a6bb89e4855643dfc385f
Tim Wray <timwray.mail@gmail.com>
2019-09-23 15:36:27 +0200

    Adjustments to mobile menu.

--- js/main.js
+++ js/main.js
@@ -2,30 +2,6 @@ window.addEventListener('DOMContentLoaded', () => {
 
   let menuContainer = document.querySelector('.menu-container');
 
-  // click / toggle functionality for menu
-  /*
-  let menuItems = document.querySelectorAll('.menu > .menu-item-has-children');
-  menuItems.forEach(menuItem => {
-    menuItem.addEventListener('mouseover', function(e) {
-      if (!menuContainer.classList.contains('open')) {
-        e.stopPropagation();
-        menuItems.forEach(menuItemWithSubmenuToHide => {
-          if (menuItem !== menuItemWithSubmenuToHide) {
-            menuItemWithSubmenuToHide.classList.remove('active');
-          }
-        });
-        this.classList.toggle('active');
-      }
-    });
-  });
-  */
-
-  // close menu if the user clicks outside of it
-  let body = document.querySelector('body');
-  body.addEventListener('click', function(e) {
-    menuItems.forEach(menuItem => menuItem.classList.remove('active'));
-  });
-
   // click / toggle functionality for mobile menu
   let menuToggler = document.querySelector('.menu-container .menu-toggler');
   menuToggler.addEventListener('click', function(e) {
