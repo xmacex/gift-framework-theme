129b7a3ef58e0b272134a2e359c419c211c8aa97
Tim Wray <timwray.mail@gmail.com>
2019-10-23 14:14:35 +0200

    Deactivated Qualaroo survey code.

--- footer.php
+++ footer.php
@@ -58,16 +58,6 @@
     }
   ?>
 </script>
-<!-- Qualaroo for gifting.digital -->
-<script type="text/javascript">
-  var _kiq = _kiq || [];
-  (function(){
-    setTimeout(function(){
-    var d = document, f = d.getElementsByTagName('script')[0], s = d.createElement('script'); s.type = 'text/javascript';
-    s.async = true; s.src = 'https://cl.qualaroo.com/ki.js/73952/hd3.js'; f.parentNode.insertBefore(s, f);
-    }, 1);
-  })();
-</script>
 <script src="<?php echo get_stylesheet_directory_uri(); ?>/js/vendor/smooth-scroll.polyfills.js"></script>
 
 <?php wp_footer(); ?>
